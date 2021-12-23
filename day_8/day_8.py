4a9e9649b8bac03e0f0dfb89754e377c86cfdd2f
Lewis <l.irvine@bath.ac.uk>
2021-12-23 17:28:03 +0000

    day 8

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -1,4 +1,4 @@
-import copy
+import numpy as np
 
 
 def part_1():
@@ -20,38 +20,71 @@ def part_1():
 def part_2():
     with open("input.txt") as f:
         data = f.read().splitlines()
-    # data = [[j for j in i.split() if j != "|"] for i in data]
-    print("data = {}".format(data))
-
     inputs = [i.split("|")[0].split() for i in data]
+    outputs = [i.split("|")[1].split() for i in data]
 
-    solution = copy.deepcopy(inputs)
+    part_2_solution = []
 
     for ix, entry in enumerate(inputs):
-        print("entry = {}".format(entry))
-        for sx, s in enumerate(entry):
-            if len(s) == 2:
-                str_1 = s
-                solution[ix][sx] = 1
-            if len(s) == 3:
-                str_7 = s
-                solution[ix][sx] = 7
-            if len(s) == 4:
-                str_4 = s
-                solution[ix][sx] = 4
-            if len(s) == 7:
-                str_8 = s
-                solution[ix][sx] = 8
+        solutions = [""] * 10
 
-        freqs = [sum([i in j for j in entry]) for i in "abcdef"]
-        print("freqs = {}".format(freqs))
         # "f" solution is whatever character is missing from only one string
+        freqs = [sum(i in j for j in entry) for i in "abcdefg"]
+        str_f = "abcdefg"[np.argmax(freqs)]
 
-        # "a" solution is whatever's missing between 7 and 1 strings
-        str_a = [i for i in str_7 if i not in str_1][0]
-        print("str_a = {}".format(str_a))
+        # special numbers with unique number of segs
+        for s in entry:
+            # 1 has two segs
+            if len(s) == 2:
+                solutions[1] = s
+            # 7 has three segs
+            if len(s) == 3:
+                solutions[7] = s
+            # 4 has four segs
+            if len(s) == 4:
+                solutions[4] = s
+            # 8 has all seven segs
+            if len(s) == 7:
+                solutions[8] = s
 
-        print("solution = {}".format(solution[ix]))
+        # deduce the others
+        for s in entry:
+            if len(s) == 5:
+                # 2 has five segs and no f seg
+                if str_f not in s:
+                    solutions[2] = s
+                # 3 has five segs and all the segs in 1
+                elif all(i in s for i in solutions[1]):
+                    solutions[3] = s
+                # 5 is the other five seg number
+                else:
+                    solutions[5] = s
+            if len(s) == 6:
+                # 9 has six segs and all the segs in 4
+                if all(i in s for i in solutions[4]):
+                    solutions[9] = s
+                # 0 has six segs and all the segs in 7 (and isn't 9)
+                elif all(i in s for i in solutions[7]):
+                    solutions[0] = s
+                # 6 is the other six seg number
+                else:
+                    solutions[6] = s
+
+        # now solve output
+        solution = ""
+        for output in outputs[ix]:
+            # how many shared segs?
+            overlaps = [sum(s in i for s in output) for i in solutions]
+            # is number of shared segs equal to len of output *and* solution?
+            matches = [
+                overlaps[i] == len(output) and len(output) == len(solutions[i])
+                for i in range(len(solutions))
+            ]
+            # add the only index that is True
+            solution += str(np.argmax(matches))
+        part_2_solution.append(int(solution))
+
+    print("part 2 answer = {}".format(sum(part_2_solution)))
 
 
 if __name__ == "__main__":
